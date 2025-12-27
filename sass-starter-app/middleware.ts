d9701b0fdf7b1ae52b73ee0f87c7eb44be2dbabe
thevaishndra <chandrav306m@gmail.com>
2025-12-27 14:36:28 +0530

    middleware role based access

--- sass-starter-app/middleware.ts
+++ sass-starter-app/middleware.ts
@@ -1,6 +1,53 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware, clerkClient } from "@clerk/nextjs/server";
+import {NextRequest} from "next/server";
+const publicRoutes = [
+"/",
+"/api/webhook/register",
+"/sign-up",
+"/sign-in"  ,
+];
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes,
+  async afterAuth(auth, req) {
+    //handle unauth users trying to access protected routes
+    if(!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url))
+    }
+
+    if(auth.userId) {
+      try {
+        const user = clerkClient.users.getUser(auth.userId);
+        const role = user.publicMetadata.role as string | undefined;
+  
+        //admin role
+        if(role === "admin" && req.nextUrl.pathname === "/dashboard"){
+          return NextResponse.redirect(new URL("/admin/dashboard", req.url))
+        }
+  
+        //prevent non admin user to access the admin routes
+        if(role != "admin" && req.nextUrl.pathname.startsWith("/admin")){
+          return NextResponse.redirect(new URL("/dashboard", req.url));
+        }
+  
+        //redirect auth user trying to access public routes
+        if(publicRoutes.includes(req.nextUrl.pathname)) {
+          return NextResponse.redirect(
+            new URL(
+              role === "admin" ? "/admin/dashboard" : "/dashboard",
+              req.url
+            )
+          );
+        }
+      } catch (error) {
+        console.error(error);
+        return NextResponse.redirect(new URL("/error", req.url))
+      }
+
+
+    }
+  }
+});
 
 export const config = {
   matcher: [
